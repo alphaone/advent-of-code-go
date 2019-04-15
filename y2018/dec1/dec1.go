1daed3ee93b093cd7396081a1717246ebe089e83
Torsten Mangner <tmangner@inoxio.de>
2019-04-15 12:07:32 +0200

    Add 2018 / day1 / part B

--- y2018/dec1/dec1.go
+++ y2018/dec1/dec1.go
@@ -6,10 +6,6 @@ import (
 	"strings"
 )
 
-func Do() string {
-	return "yup"
-}
-
 func SumUp(freqs []int) int {
 	res := 0
 
@@ -18,6 +14,22 @@ func SumUp(freqs []int) int {
 	}
 
 	return res
+}
+
+func SumUpB(freqs []int) int {
+	res := 0
+	seen := map[int]bool{0: true}
+
+	for {
+		for _, v := range freqs {
+			res += v
+			if _, ok := seen[res]; !ok {
+				seen[res] = true
+			} else {
+				return res
+			}
+		}
+	}
 }
 
 func readInput() []int {
--- y2018/dec1/dec1_test.go
+++ y2018/dec1/dec1_test.go
@@ -4,22 +4,44 @@ import (
 	"testing"
 )
 
-func test(t *testing.T, input []int, expected int) {
+func checkA(t *testing.T, input []int, expected int) {
 	actual := SumUp(input)
 	if actual != expected {
 		t.Errorf("Expected %v but got %v", expected, actual)
 	}
 }
 
-func Test(t *testing.T) {
-	test(t, []int{1, 1, 1}, 3)
-	test(t, []int{1, 1, -2}, 0)
-	test(t, []int{-1, -2, -3}, -6)
+func TestA(t *testing.T) {
+	checkA(t, []int{1, 1, 1}, 3)
+	checkA(t, []int{1, 1, -2}, 0)
+	checkA(t, []int{-1, -2, -3}, -6)
 }
 
 func TestSolvePuzzleA(t *testing.T) {
 	actual := SumUp(readInput())
 	expected := 520
+	if actual != expected {
+		t.Errorf("Expected %v but got %v", expected, actual)
+	}
+}
+
+func checkB(t *testing.T, input []int, expected int) {
+	actual := SumUpB(input)
+	if actual != expected {
+		t.Errorf("Expected %v but got %v", expected, actual)
+	}
+}
+
+func TestB(t *testing.T) {
+	checkB(t, []int{1, -1}, 0)
+	checkB(t, []int{+3, +3, +4, -2, -4}, 10)
+	checkB(t, []int{-6, +3, +8, +5, -6}, 5)
+	checkB(t, []int{+7, +7, -2, -7, -4}, 14)
+}
+
+func TestSolvePuzzleB(t *testing.T) {
+	actual := SumUpB(readInput())
+	expected := 394
 	if actual != expected {
 		t.Errorf("Expected %v but got %v", expected, actual)
 	}
